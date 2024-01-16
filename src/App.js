19ed1d26470a01c8df18d01f7b1975605c422607
Taz Whitaker <tazwhitaker@Tazs-MacBook-Air.local>
2024-01-16 10:31:03 -0500

    first commit with files and folders

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Footer, Blog, Possibility, Features, WhatGPT, Header } from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+const App = () => {
+    return (
+        <div>
+            <h1>ChatGPT</h1>
+        </div>
+    )
+}
+
+export default App
