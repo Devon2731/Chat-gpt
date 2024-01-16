a4aa90811e56e8e8dc60a8ec5a630650feff85f9
Taz Whitaker <tazwhitaker@Tazs-MacBook-Air.local>
2024-01-16 11:10:28 -0500

    added App.css and Index.css files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Footer, Blog, Possibility, Features, WhatGPT, Header } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
+import './App.css';
+import './index.css';
 const App = () => {
     return (
-        <div>
-            <h1>ChatGPT</h1>
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+            
         </div>
     )
 }
